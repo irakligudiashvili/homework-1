4f719bcf9fd49528f99cfb2c166dbdf8570a3eef
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-08-06 21:47:57 +0400

    Refactor code based on feedback
    - Remove dead code
    - Remove magic number
    - Add more edge cases to tests
    - Implement functools.total_ordering
    - Ignore metadata in comparisons
    - Adjust code to match PEP8

--- main.py
+++ main.py
@@ -1,86 +1,103 @@
-# import re
+from functools import total_ordering
 
-# re_pattern = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
 
+@total_ordering
 class Version:
     semantic_hierarchy = {
         "alpha": 1,
-        "a": 1,
         "beta": 2,
-        "b": 2,
         "rc": 3
     }
 
     def __init__(self, version):
-        # match = re.match(re_pattern, version)
-
-        # if not match:
-        #     raise ValueError(f'invalid syntax: {version}')
-
         self.version = version
         self.parts = self.parse(version)
-        # print(self.parts)
 
     def parse(self, version):
-        parts = []
+        core = []
+        prerelease = []
 
-        # replace a & b
-        if version[-2].isdigit() and not version[-1].isdigit():
-            match version[-1]:
-                case 'a':
-                    version = version[:-1] + '-alpha'
-                case 'b':
-                    version = version[:-1] + '-beta'
-                case default:
-                    raise ValueError(f'invalid syntax: {version}')
+        if '+' in version:
+            version = version.split('+')[0]
 
-        # split if prerelease
+        for suffix in ['a', 'b', 'rc']:
+            index = version.find(suffix)
+            if index > 0 and version[index - 1].isdigit():
+                match suffix:
+                    case 'a':
+                        replace_str = 'alpha'
+                    case 'b':
+                        replace_str = 'beta'
+                    case 'rc':
+                        replace_str = 'rc'
+
+                core_part = version[:index]
+                remaining = version[index + len(suffix):]
+
+                if remaining:
+                    version = f"{core_part}-{replace_str}.{remaining}"
+                else:
+                    version = f"{core_part}-{replace_str}"
+                break
+
         if '-' in version:
-            core, prerelease = version.split('-', 1)
+            core_str, prerelease_str = version.split('-', 1)
         else:
-            core = version
-            prerelease = None
+            core_str = version
+            prerelease_str = None
 
-        for part in core.split('.'):
-            parts.append(int(part))
+        for part in core_str.split('.'):
+            core.append(int(part))
 
-        if prerelease:
-            for key in prerelease.split('.'):
+        if prerelease_str:
+            for key in prerelease_str.split('.'):
                 if key.isdigit():
-                    parts.append(int(key))
+                    prerelease.append(int(key))
                 elif key in self.semantic_hierarchy:
-                    parts.append(self.semantic_hierarchy[key])
-        else:
-            # add magic number to make full release greater than prerelease
-            parts.append(99)
+                    prerelease.append(self.semantic_hierarchy[key])
 
+        parts = [core, prerelease]
         return parts
 
+    def __eq__(self, other):
+        return self.parts == other.parts
+
     def __lt__(self, other):
-        return self.parts < other.parts
+        self_core, self_pre = self.parts
+        other_core, other_pre = other.parts
 
-    def __gt__(self, other):
-        return self.parts > other.parts
+        if self_core != other_core:
+            return self_core < other_core
 
-    def __ne__(self, other):
-        return self.parts != other.parts
+        if not self_pre and other_pre:
+            return False
+        if self_pre and not other_pre:
+            return True
+        return self_pre < other_pre
+
 
 def main():
     to_test = [
         ("1.0.0", "2.0.0"),
         ("1.0.0", "1.42.0"),
         ("1.2.0", "1.2.42"),
-        ("1.5.0", "0.25.4"),
+        ("0.25.4", "1.5.0"),
         ("1.1.0-alpha", "1.2.0-alpha.1"),
         ("1.0.0-rc.1", "1.0.0"),
         ("2.1.4", "2.1.4"),
-        ("1.2.2", "1.2.2-alpha.1")
+        ("1.2.2-alpha.1", "1.2.2"),
+        ("1.2.0", "2.4.1+build44231"),
+        ("2.1.0-beta", "2.1.0b"),
     ]
 
     for left, right in to_test:
-        assert Version(left) < Version(right), f"lt failed {left} > {right}"
-        assert Version(right) > Version(left), f"gt failed {right} < {left}"
-        assert Version(right) != Version(left), f"neq failed {right} == {left}"
+        assert Version(left) < Version(right), f"lt failed: {left} < {right}"
+        assert Version(right) > Version(left), f"gt failed: {right} > {left}"
+        assert Version(left) != Version(right), f"neq failed: {left} != {right}"
+
+        assert Version(left) <= Version(right), f"le failed: {left} <= {right}"
+        assert Version(right) >= Version(left), f"ge failed: {right} >= {left}"
+        assert Version(left) == Version(right), f"eq failed: {left} == {right}"
 
 
 if __name__ == "__main__":
